8e3a4bc3fc175242bfcb95976e8d33d6cb798f65
galago <galago@pop-os.localdomain>
2022-11-29 15:48:48 +0100

    Refine JournalEntry model

--- entries/models.py
+++ entries/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class JournalEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     journalentry_text = models.CharField(max_length=200)
     entry_date = models.DateTimeField('entry date')
 
     def __str__(self):
-        return self.journalentry_text + " at " + self.entry_date
+        return self.journalentry_text
