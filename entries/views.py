4a040c8ddf3f6cb426fe849de299d54880d13bcb
galago <galago@pop-os.localdomain>
2022-11-29 16:09:04 +0100

    adding an index view on entries

--- entries/views.py
+++ entries/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import JournalEntry
 
-def index(request):
-    return HttpResponse("Hello into your journal.")
+class IndexView(generic.ListView):
+    template_name = 'entries/index.html'
+    context_object_name = 'entries_list'
+
+    def get_queryset(self):
+        return JournalEntry.objects.all()
+
