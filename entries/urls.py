13601265b2c3b4563ad14a4ad5f44c0961fae73d
galago <galago@pop-os.localdomain>
2022-11-30 14:08:34 +0100

    Added permissioned user list view

--- entries/urls.py
+++ entries/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    ]
+    path('users/',views.UserListView.as_view(), name="users"), 
+] 
--- entries/views.py
+++ entries/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from .models import JournalEntry
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
+class UserListView(PermissionRequiredMixin,generic.ListView):
+    permission_required = "auth.view_user" # be careful with the exact name and prefix
+    template_name = 'auth/user_list.html'
+    model = User
+    def get_queryset(self):
+        return User.objects.all()
+
 
 class IndexView(generic.ListView):
     template_name = 'entries/index.html'
