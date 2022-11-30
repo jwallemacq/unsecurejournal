32d53726173e70fe75c2d8a2f1140545a47b02c7
galago <galago@pop-os.localdomain>
2022-11-30 18:28:18 +0100

    Intentionnally introducing a security gap (non permissioned access to user list view

--- entries/views.py
+++ entries/views.py
@@ -3,15 +3,22 @@ from django.http import HttpResponse
 from django.views import generic
 from .models import JournalEntry
 from django.contrib.auth.models import User
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-class UserListView(PermissionRequiredMixin,generic.ListView):
-    permission_required = "auth.view_user" # be careful with the exact name and prefix
+class UserListView(generic.ListView):
     template_name = 'auth/user_list.html'
     model = User
     def get_queryset(self):
         return User.objects.all()
 
+#This view should be permissioned. Only superusers should see list of users
+#Correction
+#from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+#class UserListView(PermissionRequiredMixin,generic.ListView):
+#    permission_required = "auth.view_user" # be careful with the exact name and prefix
+#    template_name = 'auth/user_list.html'
+#    model = User
+#    def get_queryset(self):
+#        return User.objects.all()
 
 class IndexView(generic.ListView):
     template_name = 'entries/index.html'
