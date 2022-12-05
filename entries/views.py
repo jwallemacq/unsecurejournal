b77adb4c27888a438da5d5b9feae695ceea55e1a
galago <galago@pop-os.localdomain>
2022-12-05 16:51:52 +0100

    Fix index allowing super users to view all posts and normal users only their own posts

--- entries/views.py
+++ entries/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views import generic
 from .models import JournalEntry
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class UserListView(generic.ListView):
     template_name = 'auth/user_list.html'
@@ -12,7 +13,7 @@ class UserListView(generic.ListView):
 
 #This view should be permissioned. Only superusers should see list of users
 #Correction
-#from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+#from django.contrib.auth.mixins import PermissionRequiredMixin
 #class UserListView(PermissionRequiredMixin,generic.ListView):
 #    permission_required = "auth.view_user" # be careful with the exact name and prefix
 #    template_name = 'auth/user_list.html'
@@ -20,10 +21,13 @@ class UserListView(generic.ListView):
 #    def get_queryset(self):
 #        return User.objects.all()
 
-class IndexView(generic.ListView):
+class IndexView(LoginRequiredMixin,generic.ListView):
     template_name = 'entries/index.html'
     context_object_name = 'entries_list'
 
     def get_queryset(self):
-        return JournalEntry.objects.all()
+        if self.request.user.is_superuser:
+            return JournalEntry.objects.all()
+        else:
+            return JournalEntry.objects.filter(user=self.request.user)
 
